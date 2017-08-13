c6e83f69965bbd2f5739a8bea89a9e090d1abf9f
tinathomas123 <30767165+tinathomas123@users.noreply.github.com>
2017-08-13 21:01:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,14 +86,14 @@ app.get('/:articleName', function (req, res) {
   res.send(createHtmlTemplate(articles[articleName]));
 });
 
-var commentsButton=document.getElementById('commentsButton');
+/*var commentsButton=document.getElementById('commentsButton');
 commentsButton.onclick=function(){
     var comments=document.getElementById('comments');
     var commentContent=comments.innerHTML;
     var display=document.getElementById('display');
     display.innerHTML=commentContent;
     
-};
+};*/
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
