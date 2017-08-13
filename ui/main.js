54c2927852100fbb5eab9d7b318e3b8afc929a44
tinathomas123 <30767165+tinathomas123@users.noreply.github.com>
2017-08-13 21:48:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var commentsButton=document.getElementById('commentsButton');
 commentsButton.onclick=function(){
     alert('hi');
     var comments=document.getElementById('comments');
-    var commentContent=comments.innerHTML;
+    var commentContent=comments.value;
     var display=document.getElementById('display');
     display.innerHTML=commentContent;
     
