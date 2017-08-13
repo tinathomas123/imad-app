d02c0b1e6bfed44c08f20908eedf0f8407b5d25b
tinathomas123 <30767165+tinathomas123@users.noreply.github.com>
2017-08-13 20:04:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,58 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles={
+    
+    articleOne:{
+        title: "Article One",
+        header: "Article One",
+        content: "This is my first article."
+    },
+    
+    articleTwo:{
+        title: "Article Two",
+        header: "Article Two",
+        content: "This is my second article."
+    },
+    
+    articleThree:{
+        title: "Article Three",
+        header: "Article Three",
+        content: "This is my third article."
+    }
+};
+
+function createHtmlTemplate(data){
+    
+    var title=data.title;
+    var content=data.content;
+    var header=data.header;
+    
+    var htmlTemplate=
+        
+        `
+        <html>
+            <header>
+                <title>
+                    ${title};
+                </title>
+            </header>
+            <body>
+                ${header}
+                <div>
+                    ${content}
+                </div>
+            </body>
+        </html>
+        
+        
+        `;
+        
+    return htmlTemplate;
+    
+    
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -15,6 +67,12 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+app.get('/:articleName', function (req, res) {
+  
+  var articleName=req.params.articleName;
+  res.send(createHtmlTemplate(articles[articleName]));
 });
 
 
