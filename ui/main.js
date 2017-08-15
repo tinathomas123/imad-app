f9dab70518705bc02e177f41ba478eba1e576fee
tinathomas123 <30767165+tinathomas123@users.noreply.github.com>
2017-08-15 12:49:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,9 +5,31 @@ alert('inside main.js');
 var commentsButton=document.getElementById('commentsButton');
 commentsButton.onclick=function(){
     alert('hi');
-    var comments=document.getElementById('comments');
-    var commentContent=comments.value;
-    var display=document.getElementById('display');
-    display.innerHTML=commentContent;
+    var comment=document.getElementById('comments');
+    var commentContent=comment.value;
+    
+    
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+                var comments=request.responseText;
+                JSON.parse(comments);
+                var displayContent='';
+                for(var i=0;i<comments.length;i++){
+                    displayContent='<br/>'+comments[i];
+                }
+                
+                var display=document.getElementById('display');
+                display.innerHTML=displayContent;
+            }
+        }
+        
+        
+    }
+    
+    request.open('GET',"http://christinethomas221.imad.hasura-app.io/articeOne");
+    request.send(null);
+    
     
 };
