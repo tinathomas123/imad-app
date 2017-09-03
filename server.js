599f316e21ee205f6bea355e56f418a11312291f
tinathomas123 <30767165+tinathomas123@users.noreply.github.com>
2017-09-03 22:37:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.post('/reg-user',function(req,res){
     
     var dbString=hash(password,salt);
     
-    pool.query('INSERT INTO customer (username,password) VALUES($1,$2)',[username,dbString], function(err,res){
+    pool.query('INSERT INTO customer (username,password) VALUES ($1,$2)',[username,dbString], function(err,res){
        if(err){
            res.status(500).send(err.toString());
        }else{
