695cfc3b071816bc40c0b31470941af17e5ac151
tinathomas123 <30767165+tinathomas123@users.noreply.github.com>
2017-09-03 21:47:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,7 +28,7 @@ submit.onclick=function(){
     var username=document.getElementById('username');
     var password=document.getElementById('password');
     
-    request.open('POST',"http://christinethomas221.imad.hasura-app.io/reg");
+    request.open('POST',"http://christinethomas221.imad.hasura-app.io/login");
     req.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({username:'username',password:'password'}));
     
